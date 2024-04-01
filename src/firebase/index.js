870e5086af7ab25c8a34781dbe16912e6359ea9f
JelaJs <nikola4434@gmail.com>
2024-04-01 12:44:39 +0200

    Project and firebase setup

---
+++ src/firebase/index.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyAGGPtRSqlXV7unws6r6UWqnUOr5zTtg3k',
+  authDomain: 'buysellapp-ad7f2.firebaseapp.com',
+  projectId: 'buysellapp-ad7f2',
+  storageBucket: 'buysellapp-ad7f2.appspot.com',
+  messagingSenderId: '282183687634',
+  appId: '1:282183687634:web:720fae7412aa9f2fb03c0b'
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
+
+export { db }
