560a805f74235a4ad4fe01451b1857e3ebe2e23a
JelaJs <nikola4434@gmail.com>
2024-04-10 12:52:29 +0200

    Adding style, product search on homepage added.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <template>
   <header>
-    <nav>
+    <nav class="container nav">
       <RouterLink to="/">Home</RouterLink>
       <RouterLink to="/login" v-if="!isLoggedIn">Login</RouterLink>
-      <button @click="handleSignout" v-if="isLoggedIn">Sign out</button>
+      <div v-if="isLoggedIn" class="btn-link-wrap">
+        <RouterLink class="add-prod-link" to="/user">Add Product</RouterLink>
+        <button @click="handleSignout">Sign out</button>
+      </div>
     </nav>
   </header>
 
@@ -37,4 +40,40 @@ onMounted(() => {
 })
 </script>
 
-<style scoped></style>
+<style scoped>
+.nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+}
+
+.nav a {
+  text-decoration: none;
+  color: #000;
+  font-size: 2.1rem;
+}
+
+.nav a.add-prod-link {
+  padding: 0.5rem 1rem;
+  background-color: rgb(106, 106, 255);
+  color: #fff;
+  border-radius: 20px;
+}
+
+.btn-link-wrap {
+  display: flex;
+  align-items: center;
+  gap: 2rem;
+}
+
+.nav .btn-link-wrap button {
+  border: none;
+  background-color: transparent;
+  color: #000;
+  font-size: 2.1rem;
+  font-weight: 400;
+  cursor: pointer;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,26 @@
 <template>
   <main>
-    <h1>Home page</h1>
-    <ul v-if="prodRef">
-      <li v-for="product in products" :key="product.id" @click="goTosingleProduct(product.id)">
-        <img :src="product.imageUrl" :alt="product.imageName" />
-        <p>{{ product.productType }}</p>
-        <p>{{ product.productName }}</p>
-        <p>{{ product.productDescription }}</p>
-        <p>{{ product.productPrice }}</p>
-      </li>
-    </ul>
+    <div class="container">
+      <h1>Buy and Sell Quickly</h1>
+      <div class="findProd-wrap">
+        <p>Find Product:</p>
+        <input
+          v-model="searchProduct"
+          type="text"
+          placeholder="product name"
+          @input="searchForProduct"
+        />
+      </div>
+      <ul v-if="prodRef">
+        <li v-for="product in products" :key="product.id" @click="goTosingleProduct(product.id)">
+          <img :src="product.imageUrl" :alt="product.imageName" />
+          <p class="prod-type">{{ product.productType }}</p>
+          <p class="prod-name">{{ product.productName }}</p>
+          <p class="prod-desc">{{ product.productDescription.slice(0, 30) }}...</p>
+          <p class="prod-price">{{ product.productPrice }} €</p>
+        </li>
+      </ul>
+    </div>
   </main>
 </template>
 
@@ -29,18 +40,21 @@ import {
   addDoc,
   onSnapshot,
   query,
-  orderBy
+  orderBy,
+  where
 } from '@/firebase'
 
 const router = useRouter()
 const products = ref([])
-const dateSortedProducts = ref([])
-const priceSortedProducts = ref([])
-const priceToLowerSortedProducts = ref([])
+//const dateSortedProducts = ref([])
+//const priceSortedProducts = ref([])
+//const priceToLowerSortedProducts = ref([])
 const prodRef = collection(db, 'products')
-const sortedByDateRef = query(prodRef, orderBy('createdAt', 'desc')) //od najmladjeg ka najstarijem, ovo po difotu prvo mi je od najstarijeg do najmladjeg
-const sortedByPriceRef = query(prodRef, orderBy('productPrice')) //od najjeftinijeg do najskupljeg
-const toLowerSortedPriceRef = query(prodRef, orderBy('productPrice', 'desc'))
+//const sortedByDateRef = query(prodRef, orderBy('createdAt', 'desc')) //od najmladjeg ka najstarijem, ovo po difotu prvo mi je od najstarijeg do najmladjeg
+//const sortedByPriceRef = query(prodRef, orderBy('productPrice')) //od najjeftinijeg do najskupljeg
+//const toLowerSortedPriceRef = query(prodRef, orderBy('productPrice', 'desc'))
+const searchProduct = ref('')
+const searchTimer = ref(null)
 
 onSnapshot(prodRef, (snapshot) => {
   snapshot.docs.forEach((doc) => {
@@ -49,35 +63,90 @@ onSnapshot(prodRef, (snapshot) => {
   console.log(products.value)
 })
 
-onSnapshot(sortedByDateRef, (snapshot) => {
-  snapshot.docs.forEach((doc) => {
-    dateSortedProducts.value.push({ ...doc.data(), id: doc.id })
-  })
-  console.log('date sorted', dateSortedProducts.value)
-})
-
-onSnapshot(sortedByPriceRef, (snapshot) => {
-  snapshot.docs.forEach((doc) => {
-    priceSortedProducts.value.push({ ...doc.data(), id: doc.id })
-  })
-  console.log('price sorted', priceSortedProducts.value)
-})
-
-onSnapshot(toLowerSortedPriceRef, (snapshot) => {
-  snapshot.docs.forEach((doc) => {
-    priceToLowerSortedProducts.value.push({ ...doc.data(), id: doc.id })
-  })
-  console.log('price to lower sorted', priceToLowerSortedProducts.value)
-})
-
 const goTosingleProduct = (id) => {
   router.push(`/singleproduct/${id}`)
+}
+
+const searchForProduct = () => {
+  clearTimeout(searchTimer.value)
+  searchTimer.value = setTimeout(() => {
+    if (searchProduct.value === '') {
+      onSnapshot(prodRef, (snapshot) => {
+        snapshot.docs.forEach((doc) => {
+          products.value.push({ ...doc.data(), id: doc.id })
+        })
+      })
+
+      return
+    }
+    products.value = products.value.filter(
+      (prod) => prod.productName.trim().toLowerCase() === searchProduct.value.trim().toLowerCase()
+    )
+  }, 300)
 }
 </script>
 
 <style scoped>
-img {
-  width: 200px;
-  height: 200px;
+h1 {
+  text-align: center;
+  font-size: 4.2rem;
+}
+
+.findProd-wrap {
+  display: flex;
+  align-items: center;
+  margin-top: 3rem;
+  gap: 0.5rem;
+}
+
+.findProd-wrap p {
+  font-size: 2rem;
+}
+
+.findProd-wrap input {
+  padding: 0.5rem 1rem;
+  outline: none;
+  border-radius: 10px;
+  border: 1px solid #ccc;
+}
+
+ul {
+  list-style: none;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  gap: 3rem;
+  margin-top: 2rem;
+}
+
+ul li {
+  cursor: pointer;
+}
+
+ul li img {
+  width: 100%;
+  height: 100%;
+  border-radius: 10px;
+  object-fit: cover;
+}
+
+ul li .prod-type {
+  font-size: 1.4rem;
+  color: #00000094;
+}
+
+ul li .prod-name {
+  font-size: 2rem;
+  font-weight: 600;
+  margin-top: 0.2rem;
+  text-transform: uppercase;
+}
+
+ul li .prod-desc {
+  margin-top: 1rem;
+  font-size: 1.8rem;
+}
+
+ul li .prod-price {
+  font-size: 1.8rem;
 }
 </style>
